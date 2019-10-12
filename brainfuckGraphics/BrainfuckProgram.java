d98b61d0ee26bd890398ac69f7523c6b8f5d3c45
Haschenohren <enmoniz@uncg.edu>
2019-10-12 16:20:31 -0400

    moved stuff again sorry

---
+++ brainfuckGraphics/BrainfuckProgram.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package brainfuckGraphics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Eva Moniz
+ */
+public class BrainfuckProgram {
+
+    private String code;
+    private Map<Integer, Integer> matchingBrackets;
+
+    public BrainfuckProgram(String code) {
+        this.code = code;
+        this.matchingBrackets = new HashMap<>();
+    }
+}
